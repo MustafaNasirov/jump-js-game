f8653791abe126294b8be7271b43cb7285731b01
MustafaNsrv <86935457+MustafaNasirov@users.noreply.github.com>
2022-07-13 19:13:51 +0400

    added free movement and fixed start screen formatting

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
-
-// player jump
-// obstacle movement
-// obstacle collision
-// start menu / score
-// obstacle variation + movement
-
-
 // variable declarations
 var isGameStart = false;
 
 // DOM declarations
 const player = document.getElementById("player");
-const floor = document.getElementById("floor");
 const obstacle = document.getElementById("obstacle");
+//const floor = document.getElementById("floor");
 
 const tutorialScreen = document.getElementById("tutorial-screen");
 const gameScreen = document.getElementsByClassName("game-container");
@@ -20,10 +12,8 @@ const gameScreen = document.getElementsByClassName("game-container");
 const startGame = () => {
     isGameStart = true;
     obstacle.classList.add("moveObstacle");
-    gameScreen.style.display = "block";
-    
-    tutorialScreen.style.display = "none";
 
+    hideTutorial();
 };
 
 const hideTutorial = () =>{
@@ -32,14 +22,10 @@ const hideTutorial = () =>{
 
 // Animate player element to jump
 const playerJump = () => {
-
-    console.log(player.classList);
     // If player is already in jump animation (jump animation class is attached)
     if(player.classList === true){
-        
         return;
     }
-
     // otherwise play animation
     player.classList.add("animateJump");
     setTimeout(removeJump, 750);
@@ -50,7 +36,64 @@ const removeJump = () =>{
     player.classList.remove("animateJump");
 };
 
-const removeMovement = () =>{
+// Checks for collisions every 5ms
+const checkCollision = setInterval(() => {
+
+    // pixel position of player element 
+    let playerTop = parseInt(window.getComputedStyle(player).getPropertyValue("top"));
+    let playerLeft = parseInt(window.getComputedStyle(player).getPropertyValue("left"));
+  
+    // pixel position of obstacle element 
+    let obstacleTop = parseInt(window.getComputedStyle(obstacle).getPropertyValue("top"));
+    let obstacleLeft = parseInt(window.getComputedStyle(obstacle).getPropertyValue("left"));
+    
+    // check if elements overlap (if player is inside obstacle there is a collision)
+    if(((obstacleLeft < (playerLeft + 48)) && (obstacleLeft > playerLeft)) && ((playerTop) > obstacleTop)){
+        //alert("Game Over!");
+    }
+}, 5);
+
+
+const handleKeyPress = (event) => {
+    if(!isGameStart){ 
+        startGame() };
+
+    var key = event.key;
+
+    if (key == 'w' || key == ' ') {
+        //move up
+        playerJump();
+    }
+    else if (key == 'a') {
+        //move left
+        moveElement(player, -10);
+    }
+    else if (key == 'd') {
+        // move right
+        moveElement(player, 10);
+    }
+};
+
+// Function to change position pixel value of element
+const moveElement = (element, pixels) =>{
+    let newVal = (parseInt(window.getComputedStyle(element).getPropertyValue("left"))) + pixels;
+    newVal = newVal.toString() + "px";
+
+    player.style.left = newVal;
+    return newVal;
+}; 
+
+
+
+// Event listeners
+document.addEventListener('click', playerJump);
+document.addEventListener('keypress', (e) => {
+    handleKeyPress(e);
+});
+
+
+
+/* const removeMovement = () =>{
     player.classList.remove("animatePlayerRight");
     player.classList.remove("animatePlayerLeft");
 }
@@ -67,64 +110,4 @@ const playerMovementLeft = () => {
 
     player.classList.add("animatePlayerLeft")
     setTimeout(removeMovement, 300);
-}; 
-
-
-// Checks for collisions every 5ms
-const checkCollision = setInterval(() => {
-
-    // pixel position of player element 
-    let playerTop = parseInt(window.getComputedStyle(player).getPropertyValue("top"));
-    let playerLeft = parseInt(window.getComputedStyle(player).getPropertyValue("left"));
-   //console.log("player L,Top,R : " + playerLeft + ", " + playerTop + ", " + playerRight);
-   //console.log("p " + playerTop)
-  
-    // pixel position of obstacle element 
-    let obstacleTop = parseInt(window.getComputedStyle(obstacle).getPropertyValue("top"));
-    let obstacleLeft = parseInt(window.getComputedStyle(obstacle).getPropertyValue("left"));
-   //console.log("obstacle L,Top,R : " + obstacleLeft + ", " + obstacleTop + ", " + obstacleRight);
-   //console.log("o " + obstacleLeft);
-    
-    // check if elements overlap (if player is inside obstacle there is a collision)
-    if((obstacleLeft < (playerLeft + 48)) && (obstacleLeft > playerLeft) && ((playerTop - 48) > obstacleTop)){
-        console.log("collision")
-
-        alert("Game Over!");
-    }
-}, 5);
-
-/* const convertRem = (str) =>{
-    str.replace()
-
-}; */
-
-
-const handleKeyPress = (event) => {
-    if(!isGameStart){ startGame() };
-
-    var key = event.key;
-    console.log(key);
-
-    if (key == 'w' || key == ' ') {
-        playerJump();
-        // up arrow
-    }
-    else if (key == 'a') {
-        //playerMovementLeft();
-        player.style.left = "3rem";
-       // left arrow
-    }
-    else if (key == 'd') {
-        //playerMovementRight();
-        player.style.left = "6rem";
-       // right arrow
-    }
-};
-
-// Event listeners
-document.addEventListener('click', playerJump);
-document.addEventListener('keypress', (e) => {
-    handleKeyPress(e);
-});
-
-console.log(tutorialScreen.style)
+};  */
